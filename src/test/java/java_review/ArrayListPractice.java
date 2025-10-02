ce9fcd8abbff75c6811abb00f91af13797e7eb3c
Murodil <murodil@cybertekschool.com>
2025-10-02 19:20:09 -0400

    Java review day02: arraylist

--- src/test/java/java_review/ArrayListPractice.java
+++ src/test/java/java_review/ArrayListPractice.java
@@ -60,4 +60,19 @@ public class ArrayListPractice {
     /*
     fizzBuzzList(): returns Arraylist with fizzbuzz logic
      */
+    public static ArrayList<String> fizzBuzzList() {
+        ArrayList<String> result = new ArrayList<>();
+        for(int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(i+"");
+            }
+        }
+        return result;
+    }
 }
