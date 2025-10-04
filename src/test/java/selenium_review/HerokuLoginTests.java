2dedfd111aa3c3108dcf54b0623b91e71a983197
Murodil <murodil@cybertekschool.com>
2025-10-04 11:58:11 -0400

    Selenium review day01

--- src/test/java/selenium_review/HerokuLoginTests.java
+++ src/test/java/selenium_review/HerokuLoginTests.java
@@ -1,7 +1,10 @@
 package selenium_review;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -25,6 +28,16 @@ public class HerokuLoginTests {
         driver.get(loginUrl);
         System.out.println("Title = " + driver.getTitle());
         //verify correct page by checking the title "The Internet"
+        Assert.assertEquals(driver.getTitle(), "The Internet");
+        //locate username field and enter "tomsmith"
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
 
+        //locate password field and enter "SuperSecretPassword!"
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("SuperSecretPassword!");
+
+        //click on login button
+        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginBtn.click();
     }
 }
