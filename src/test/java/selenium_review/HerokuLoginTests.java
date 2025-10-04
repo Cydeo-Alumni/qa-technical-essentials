73a5b093c99a4e1c980e5387f719ec59d67c3de4
Murodil <murodil@cybertekschool.com>
2025-10-04 13:02:01 -0400

    Selenium review day01

--- src/test/java/selenium_review/HerokuLoginTests.java
+++ src/test/java/selenium_review/HerokuLoginTests.java
@@ -29,7 +29,7 @@ public class HerokuLoginTests {
         //driver.quit();
     }
 
-    @Test
+    @Test(priority = 1)
     public void validCredentialsLoginTest() {
         driver.get(loginUrl);
         System.out.println("Title = " + driver.getTitle());
@@ -59,7 +59,7 @@ public class HerokuLoginTests {
     Enter omsmith  | SuperSecretPasswo
     and verify "Your password is invalid!" is displayed
      */
-    @Test
+    @Test(priority = 2)
     public void invalidUsernameTest() {
         driver.get(loginUrl);
         System.out.println("Title = " + driver.getTitle());
@@ -80,5 +80,29 @@ public class HerokuLoginTests {
         System.out.println(invalidUsernameElem.getText());
         Assert.assertTrue(invalidUsernameElem.isDisplayed());
         Assert.assertTrue(invalidUsernameElem.getText().contains("Your username is invalid!"));
+    }
+
+    //| tomsmith  | SupersecretPassword!   | Your password is invalid!
+    @Test(priority = 3)
+    public void invalidPasswordTest() {
+        driver.get(loginUrl);
+        System.out.println("Title = " + driver.getTitle());
+        //verify correct page by checking the title "The Internet"
+        Assert.assertEquals(driver.getTitle(), "The Internet");
+        //locate username field and enter "tomsmith"
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+
+        //locate password field and enter "SuperSecretPassword!"
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("SupersecretPassword!");
+
+        //click on login button
+        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginBtn.click();
+
+        WebElement invalidPasswordElem = driver.findElement(By.xpath("//div[@class='flash error']"));
+        System.out.println(invalidPasswordElem.getText());
+        Assert.assertTrue(invalidPasswordElem.isDisplayed());
+        Assert.assertTrue(invalidPasswordElem.getText().contains("Your password is invalid!"));
     }
 }
