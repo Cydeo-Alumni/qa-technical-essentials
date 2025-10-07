51f8137bd355c5a46f2ff0c0f3effa806f9207ed
Murodil <murodil@cybertekschool.com>
2025-10-07 19:00:09 -0400

    Selenium review day02

--- src/test/java/selenium_review/PracticeTestAutomationLoginTests.java
+++ src/test/java/selenium_review/PracticeTestAutomationLoginTests.java
@@ -3,6 +3,8 @@ package selenium_review;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -29,5 +31,28 @@ public class PracticeTestAutomationLoginTests {
         Assert.assertTrue(driver.findElement(By.xpath("//h1[.='Logged In Successfully']")).isDisplayed());
 
         Assert.assertTrue(driver.findElement(By.linkText("Log out")).isDisplayed());
+
+        driver.quit();
+    }
+
+    @Test
+    public void invalidUsernameTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        driver.findElement(By.id("username")).sendKeys("incorrectUser");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+
+        driver.findElement(By.id("submit")).click();
+
+        //explicit wait for error message to be visible
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("error"))));
+
+        Assert.assertTrue(driver.findElement(By.id("error")).isDisplayed());
+
+        Assert.assertEquals(driver.findElement(By.id("error")).getText(), "Your username is invalid!");
     }
 }
